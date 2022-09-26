7621d7f956335567990ec7e0791c410f4d01a928
Sakib Hasan <sakib100.sa@gmail.com>
2022-09-27 03:16:45 +0600

    exercise 10

--- index.js
+++ index.js
@@ -104,3 +104,17 @@ const sortedNumbers = (array) => {
 };
 
 console.log(sortedNumbers([16, 22, 3, 14, 25, 76, 107, 58, 99]));
+
+// Exercise 10
+//----> <----
+// Write a js program to find if the year is a leap year or not.
+
+const isLeapYear = (year) => {
+	if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+		return `${year} is a leap year`;
+	} else {
+		return `${year} is not a leap year`;
+	}
+};
+
+console.log(isLeapYear(2024));

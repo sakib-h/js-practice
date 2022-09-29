ae9954845f40c7090c5dcc4e22f8ba4f88d28a7c
Sakib Hasan <sakib100.sa@gmail.com>
2022-09-29 15:48:21 +0600

    little change

--- 11-20.js
+++ 11-20.js
@@ -44,3 +44,22 @@ const longestString = (names) => {
 };
 
 console.log(longestString(["Sakib", "Hasan", "Sakib Hasan"]));
+
+// --> Practice 14
+// Write a js program to find which number is divided by 3 or 5 or both
+
+const fizzBuzz = (start, end) => {
+	for (let i = start; i <= end; i++) {
+		if (i % 15 === 0) {
+			console.log("FizzBuzz");
+		} else if (i % 3 === 0) {
+			console.log("Fizz");
+		} else if (i % 5 === 0) {
+			console.log("Buzz");
+		} else {
+			console.log(i);
+		}
+	}
+};
+
+fizzBuzz(1, 30);
